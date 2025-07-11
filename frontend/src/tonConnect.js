36306d74c984584c4bbda956690b3186b318b5f2
Vadzimik977 <136469818+Vadzimik977@users.noreply.github.com>
2025-07-11 16:56:33 +0300

    deleted frontend

--- frontend/src/tonConnect.js
+++
@@ -1,7 +0,0 @@
-import { TonConnect } from '@tonconnect/sdk';
-
-const dappMetadata = {
-    manifestUrl: './ton-manifest.json'
-}
-
-export const connector = new TonConnect(dappMetadata)
--- frontend/src/utils/utils.js
+++
@@ -1,7 +0,0 @@
-export function isMobile() {
-	return window.innerWidth <= 500;
-}
-
-export function isDesktop() {
-	return window.innerWidth >= 1050;
-}
