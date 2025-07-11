aa936f3849f8e17756bac372e33c7467234644b7
Vadzimik977 <136469818+Vadzimik977@users.noreply.github.com>
2025-07-11 16:00:53 +0300

    Create config.py

---
+++ backend/config.py
@@ -0,0 +1,9 @@
+import os
+from typing import List
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+BASE_SITE = "https://playmost.ru"
+
+
+API_TOKEN = "6903599422:AAG1BPqlaxdx38FA2vPOd5vJCgJ36XUcl1Y"
